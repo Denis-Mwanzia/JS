a85c3b26d307fc20787d2cfe1f008d2514119dc2
Denis Mwanzia <denismwanzia56@gmail.com>
2025-05-20 03:16:41 +0300

    Added type coercion content

---
+++ DAY2/coercion.js
@@ -0,0 +1,12 @@
+// Type coercion is the process of implicitly converting on type to another during operation like addition or comparison.
+// Example
+console.log('4' - 2); //2
+console.log('4' + 2); //42
+console.log(true + false); //1
+console.log(null == undefined); //true
+console.log(null === undefined); //false
+console.log('5' == 5); //true
+console.log('5' === 5); //false
+console.log(Boolean('')); //false
+console.log(Boolean('false')); //true
+console.log(10 + true); //11
