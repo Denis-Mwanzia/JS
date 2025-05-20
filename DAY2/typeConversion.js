a6e522429213938c5443ded432fc02eb6bede5d4
Denis Mwanzia <denismwanzia56@gmail.com>
2025-05-20 03:08:29 +0300

    type conversion content

---
+++ DAY2/typeConversion.js
@@ -0,0 +1,7 @@
+// Type conversion is the process of explicitly making conversion from one data type to another
+// Example
+console.log(Number('42')); // string to number
+console.log(String(100)); // number to string
+console.log(Boolean(0.1)); // number to boolean. 0 returns False while non-zero result to True
+console.log(parseInt('23.45')); // string to integer
+console.log(parseFloat('23.45')); // string to decimal
